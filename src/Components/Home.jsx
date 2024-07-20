a7368f543901d3fbc5de2f2354b8d6350f04fc1d
ShyamaAgrawal <shyama.agrawal162@gmail.com>
2024-07-20 15:32:27 +0530

    update

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
           Our platform utilizes advanced machine learning algorithms to analyze your health data and provide personalized insights into your lung cancer risk. Early detection is crucial for successful treatment, and our tool empowers you to take proactive steps towards your health.
         </p>
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" onClick={(e)=>{navigate('/details')}}>
-          Analyse Yourself
+          Analyze Yourself
         </button>
       </div>
       <div className="healthTips"></div>
