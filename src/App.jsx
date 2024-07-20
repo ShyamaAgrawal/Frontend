42c377fc8788d091b048825484642a57c6d836a7
ShyamaAgrawal <shyama.agrawal162@gmail.com>
2024-07-20 12:25:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Login from "./Components/Login";
+import Signup from "./Components/Signup";
+
+import Home from "./Components/Home";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Signup />} />
+    </Routes>
+  );
+};
+
+export default App;
